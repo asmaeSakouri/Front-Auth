8b8af2c50fa4d7fb4468ddd0205089a77803901e
asmaeSakouri <asmaesakouri5@gmail.com>
2023-12-13 23:59:20 +0100

    added logout

---
+++ src/app/guards/autorisation.guard.ts
@@ -0,0 +1,32 @@
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanActivateFn,
+  Router,
+  RouterStateSnapshot,
+  UrlTree
+} from '@angular/router';
+
+import {AuthService} from "../services/auth.service";
+import {Observable} from "rxjs";
+import {Injectable} from "@angular/core";
+@Injectable({
+  providedIn:"root"
+})
+export class AutorisationGuard implements CanActivate{
+  constructor(private authService:AuthService,
+              private router:Router) {
+  }
+  canActivate(route:ActivatedRouteSnapshot
+    , state:RouterStateSnapshot
+  ):Observable<boolean | UrlTree> | Promise<boolean|UrlTree>|boolean  {
+   if (this.authService.roles.includes("ADMIN")){
+     return true;
+   }
+   else{
+     this.router.navigateByUrl("/admin/not-authorized")
+      return false;
+   }
+  }
+}
+
